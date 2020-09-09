2363050d4f1af09a367d90c94a8c0ed39800ff98
Daniel Rochetti <daniel.rochetti@gmail.com>
2020-09-09 13:16:52 -0700

    add kingfisher integration further docs

--- AmplifyExtensions/Kingfisher/ImageDataProvider+Amplify.swift
+++ AmplifyExtensions/Kingfisher/ImageDataProvider+Amplify.swift
@@ -10,17 +10,27 @@
 import Amplify
 import Kingfisher
 
+/// `ImageDataProvider` implementation that loads images using `Amplify.Storage`.
 public struct AmplifyImageProvider: ImageDataProvider {
-    public let cacheKey: String
+    public let key: String
     public let accessLevel: StorageAccessLevel
 
+    public var cacheKey: String {
+        "\(accessLevel.rawValue)/\(key)"
+    }
+
     public func data(handler: @escaping (Result<Data, Error>) -> Void) {
+        Amplify.Logging.verbose("""
+        Kingfisher will load an image using Amplify.Storage with key \(cacheKey) and access level \(accessLevel)
+        """)
         let options = StorageDownloadDataRequest.Options(accessLevel: accessLevel)
-        _ = Amplify.Storage.downloadData(key: cacheKey, options: options) { result in
+        _ = Amplify.Storage.downloadData(key: key, options: options) { result in
             switch result {
             case .success(let data):
+                Amplify.Logging.verbose("Success loading image data for Kingfisher using Amplify.Storage")
                 handler(.success(data))
             case .failure(let error):
+                Amplify.Logging.error(error: error)
                 handler(.failure(error))
             }
         }
@@ -29,8 +39,29 @@ public struct AmplifyImageProvider: ImageDataProvider {
 
 extension Source {
 
+    /// Returns a `.provider(ImageDataProvider)` setup with the Amplify integration.
+    ///
+    /// UIKit example:
+    ///
+    /// ```swift
+    /// let imageView = ImageView()
+    /// imageView.kf.setImage(with: .amplify(key: "myimage", accessLevel: .guest))
+    /// ```
+    ///
+    /// SwiftUI example:
+    ///
+    /// ```swift
+    /// VStack {
+    ///     KFImage(source: .amplify(key: "myimage", accessLevel: .guest))
+    /// }
+    /// ```
+    ///
+    /// - Parameters
+    ///   - key: the stored image identifier.
+    ///   - accessLevel: the access level (defaults to `.guest`)
+    /// - Returns: `.provider(ImageDataProvider)` using `AmplifyImageProvider`
     public static func amplify(key: String, accessLevel: StorageAccessLevel = .guest) -> Source {
-        return .provider(AmplifyImageProvider(cacheKey: key, accessLevel: accessLevel))
+        return .provider(AmplifyImageProvider(key: key, accessLevel: accessLevel))
     }
 
 }
